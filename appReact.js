666a255be608469d45da48bc1ea3545813938e40
samuel-kar <samuel.karlhager@hotmail.com>
2025-03-19 13:27:20 +0100

    adding last steps i.e. hooks

--- appReact.js
+++ appReact.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 function MyButton() {
     return (
         <button>I'm a button</button>
@@ -44,3 +46,75 @@ const user = {
       </>
     );
   }
+
+  const products = [
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 3 },
+  ];
+  
+  export default function ShoppingList() {
+    const listItems = products.map(product =>
+      <li
+        key={product.id}
+        style={{
+          color: product.isFruit ? 'magenta' : 'darkgreen'
+        }}
+      >
+        {product.title}
+      </li>
+    );
+  
+    return (
+      <ul>{listItems}</ul>
+    );
+  }
+
+  export default function MyApp() {
+    return (
+      <div>
+        <h1>Counters that update separately</h1>
+        <MyButton />
+        <MyButton />
+      </div>
+    );
+  }
+  
+  function MyButton() {
+    const [count, setCount] = useState(0);
+  
+    function handleClick() {
+      setCount(count + 1);
+    }
+  
+    return (
+      <button onClick={handleClick}>
+        Clicked {count} times
+      </button>
+    );
+  }
+
+  export default function MyApp() {
+    const [count, setCount] = useState(0);
+  
+    function handleClick() {
+      setCount(count + 1);
+    }
+  
+    return (
+      <div>
+        <h1>Counters that update together</h1>
+        <MyButton count={count} onClick={handleClick} />
+        <MyButton count={count} onClick={handleClick} />
+      </div>
+    );
+  }
+  
+  function MyButton({ count, onClick }) {
+    return (
+      <button onClick={onClick}>
+        Clicked {count} times
+      </button>
+    );
+  }
+  
